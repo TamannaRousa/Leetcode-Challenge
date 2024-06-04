ddd1d02c9913a2c5294ee5e49cb2240ab786c7c5
TamannaRousa <tamanna.rousa.cs25@iilmcet.ac.in>
2024-06-04 14:32:41 +0530

    longest palindrome | leetcode 406 | java | EASY

---
+++ Leetcode409.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+
+public class Leetcode409 {
+
+    public static int longestPalindrome(String s){
+        HashSet<Character> charSet = new HashSet<>();
+
+        int length = 0;
+        for(char c : s.toCharArray()){
+            if(charSet.contains(c)){
+                charSet.remove(c);
+                length+=2;
+            }
+            else{
+                charSet.add(c);
+            }
+        }
+        if(!charSet.isEmpty()){
+            length+=1;
+        }
+        return length;
+    }
+    public static void main(String[] args) {
+        String s = "abccccdd";
+        System.out.println(longestPalindrome(s)); // Output: 7
+    }
+}
