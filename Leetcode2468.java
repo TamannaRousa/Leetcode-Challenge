a7cf701db4253a6286a329429b35b0460270ed6f
TamannaRousa <tamanna.rousa.cs25@iilmcet.ac.in>
2024-06-09 23:00:26 +0530

    Subarray Sums Divisible by K | Leetcode challenge 974 | MEDIUM | JAVA

---
+++ Leetcode2468.java
@@ -0,0 +1,17 @@
+// 2486. Append Characters to String to Make Subsequence
+public class Leetcode2468 {
+        public int appendCharacters(String s, String t) {
+            int sIndex = 0, tIndex = 0;
+            int sLength = s.length(), tLength = t.length();
+            
+            while (sIndex < sLength && tIndex < tLength) {
+                if (s.charAt(sIndex) == t.charAt(tIndex)) {
+                    tIndex++;
+                }
+                sIndex++;
+            }
+            
+            return tLength - tIndex;
+            
+        }
+}
