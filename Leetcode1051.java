5b5748b38368952ccbcfcf70a9ff8c4d27a723c4
TamannaRousa <tamanna.rousa.cs25@iilmcet.ac.in>
2024-06-10 21:27:37 +0530

    Height Checkers | Leetcode challenge 1051 | EASY | JAVA

---
+++ Leetcode1051.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Leetcode1051 {
+    public int heightChecker(int[] heights) {
+        int count=0;
+        int[] expected = heights.clone();
+        Arrays.sort(expected);
+
+        for(int i=0;i<heights.length;i++){
+            if(heights[i] != expected[i]){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
